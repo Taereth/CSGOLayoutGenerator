7a21e1c2b4952cfd2893a0d483b9c76dbb73f661
Taereth <orell.endres@stud.hslu.ch>
2019-11-15 16:06:08 +0100

    Layout Generation
    Implemented a more beautful layout generation, that actually makes sense

--- _02-Final-Project/FinalProject/sketch.js
+++ _02-Final-Project/FinalProject/sketch.js
@@ -15,6 +15,8 @@ var centerVector;
 
 var areas = [];
 var rooms = [];
+var paths = [];   //Different Paths (f.e. LongA,LongB);
+var groups = [];  //groups of room vectors that should form drawn rooms together
 
 
 function setup() {
@@ -34,7 +36,7 @@ function setup() {
   defineLongPaths();
   defineMid();
 
-  //LongPaths
+  //Generate Main Paths
 
   randomPath(rooms[1].vector,rooms[2].vector);
   randomPath(rooms[1].vector,rooms[3].vector);
@@ -48,6 +50,12 @@ function setup() {
   randomPath(rooms[7].vector,rooms[8].vector);
   randomPath(rooms[0].vector,rooms[8].vector);
   randomPath(rooms[1].vector,rooms[8].vector);
+
+  //Group Points for room drawing
+
+  for(var i=0;i<paths.length;i++){
+    groupPoints(paths[i]);
+  }
 
 }
 
@@ -67,12 +75,22 @@ function draw() {
     areas[i].draw();
   }
 */
+    //THIS IS ONLY FOR TESTING ROOM PLACEMENT
+/*
   for(var i=0;i<rooms.length;i++){
     rooms[i].draw();
     stroke("black");
     text(i,rooms[i].vector.x,rooms[i].vector.y);
 
     }
+*/
+    for(var i=0;i<groups.length;i++){
+    minimalAreaRectangle(groups[i]);
+    }
+
+
+
+
 
 
 
@@ -143,12 +161,6 @@ function defineMid(){
   var Mid = createVector(randN(TLongAEdgeASite.vector.x,TLongBEdgeBSite.vector.x),randN(TSpawn.vector.y,CTSpawn.vector.y));
   Mid = new Room(Mid.x,Mid.y,20,20,"Mid");
   rooms.push(Mid);
-}
-
-
-//Only for Testing Purposes
-function drawTestLines(){
-
 }
 
 class Room{
@@ -301,6 +313,9 @@ function randomPath(start,end){
 
   // run until we hit the endpoint!
   var counter=0;
+  var path = []; //array for specific path being generated
+  path.push(start); //push start point into path array
+
   while (true) {
 
     // compute angle b/w current position and the end point
@@ -317,8 +332,14 @@ function randomPath(start,end){
     var xps = current.x + cos(newAngle) * lineLen;
     var yps = current.y + sin(newAngle) * lineLen;
 
+    //Create new room and push it into room array
+
     var connection = new Room(xps,yps,5,5,"connection");
     rooms.push(connection);
+
+    //Save Rooms to arrays based on paths for outline generation
+
+    path.push(connection.vector);
 
     // update current position to the end of the line, figure
     // out how far we have left to go
@@ -327,6 +348,8 @@ function randomPath(start,end){
 
     // if we're near the endpoint, draw a line to the end and quit
     if (distLeft <= minDistToEnd) {
+      path.push(end); //push end point into path array
+      paths.push(path); //push path array into global array
       break;
     }
 
@@ -334,5 +357,49 @@ function randomPath(start,end){
     counter += 1;
     noiseOffset += noiseInc;
   }
+
+}
+
+
+//Group Paths into smaller groups in order to draw rooms
+
+function groupPoints(path){
+
+    var size=0
+    for(var i=0;i<path.length;i+=size){
+      size=Math.floor(random(3,5));
+      var pushgroup=path.slice(i,i+size);
+      if(i!=0&&i!==1){
+        pushgroup.unshift(path[i-2],path[i-1]);
+      }
+      groups.push(pushgroup);
+    }
+
+    console.log(groups);
+
+}
+
+//Calculate smallest possible rectangle and draw it ()
+
+function minimalAreaRectangle(points){
+
+  var minimalX,minimalY;
+  var maximalX,maximalY;
+  minimalX=minimalY=0;
+  maximalX=maximalY=9999;
+  var xarray=[];
+  var yarray=[];
+  for(var i=0;i<points.length;i++){
+    xarray.push(points[i].x);
+    yarray.push(points[i].y);
+  }
+  minimalX=Math.min(...xarray);
+  maximalX=Math.max(...xarray);
+  minimalY=Math.min(...yarray);
+  maximalY=Math.max(...yarray);
+
+  noStroke();
+  fill("white");
+  quad(minimalX-5,minimalY-5,maximalX+5,minimalY-5,maximalX+5,maximalY+5,minimalX-5,maximalY+5);
 
 }
